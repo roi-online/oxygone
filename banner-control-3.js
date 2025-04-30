bde65dbbd4d2c1ba0129d6652d9d66f728ca3bcd
roi-online <mho@roi-online.dk>
2025-04-30 15:06:41 +0200

    Update banner-control-3.js

--- banner-control-3.js
+++ banner-control-3.js
@@ -33,7 +33,7 @@ var cookieInfoText = ["Expiration", "Domain", "Controller"];
 
 var bannerShowingDelayed = 0;
 
-var websiteLogo = "https://www.oxygone.dk/wp-content/uploads/2025/03/oxygone_logo-64x19.png";
+var websiteLogo = "https://www.oxygone.testprojekt.dk/wp-content/uploads/2025/03/oxygone_logo.png";
 
 var logoWidth = "200px";
 
